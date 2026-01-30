63b02b5f6b5a1e5bb3cd01d0ce715af43a087bce
FrancescoTria <francescotria26@gmail.com>
2026-01-30 17:59:54 +0100

    Issue #11 - Creazione base di frontend in react

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './styles/App.css';
+
+function App() {
+
+  return (
+    <>
+      {/* <p>ciao</p>
+      <h1>hello world</h1>
+      <button>Click me</button > */}
+    </>
+  );
+}
+
+export default App
